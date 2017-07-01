96b251c459fa5f6e1fe24ef52d318ffd5aa1353f
Your Name <denismoureu@gmail.com>
2017-07-01 21:00:41 +0200

    OK

---
+++ Assets/Scripts/Player/Item/ItemsTypes/PotionMana.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionMana : Item {
+
+	// Use this for initialization
+	void Start () {
+		base.Start ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		base.Update ();
+	}
+
+	public override bool use()
+	{
+		RPGPlayer player = RPGPlayer.Player.GetComponent<RPGPlayer> ();
+		Debug.Log ("utilisation potion mana");
+		bool rem = player.addMana ((int)(player.getMaxMana() * 0.3f));
+
+		if (rem)
+			player.removeItemToInventory (this);
+		return true;
+	}
+}
